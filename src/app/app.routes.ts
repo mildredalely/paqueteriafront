4c7a7e8b89b0d517938c3154c3e0dec390bf5bed
Jose <utti222001@utvco.edu.mx>
2025-10-23 20:01:19 -0600

    Footer funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { NuevoUsuarioComponent } from './pages/nuevo-usuario/nuevo-usuario.component';
 import { EnvioMxComponent } from './pages/envio-mx/envio-mx.component';
+import { ReporteComponent } from './pages/reporte/reporte.component';
+import { HistorialComponent } from './pages/historial/historial.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +18,14 @@ export const routes: Routes = [
     {
         path: 'envio-mx',
         component: EnvioMxComponent
+    },
+    {
+        path: 'reporte',
+        component: ReporteComponent
+    },
+    {
+        path: 'historial',
+        component: HistorialComponent
     },
     {
         path: 'login',
