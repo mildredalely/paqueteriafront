e3b7117136a0b6b4e6993d925ee820844952a276
Miriam Belen <miriamstgos@gmail.com>
2025-10-23 22:13:48 -0600

    creando ruta historial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HistorialComponent } from './pages/historial/historial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'historial',
+        component: HistorialComponent
+    },
+    // Optional: home redirect
+    {
+        path: '',
+        redirectTo: 'historial',
+        pathMatch: 'full'
+    },
+    // Wildcard fallback
+    {
+        path: '**',
+        redirectTo: 'historial'
+    }
+];
